9fe496bb36834f52bb2acc01aea70405049b5091
Devendra Sharma <pdevendra268@gmail.com>
2024-05-20 00:30:07 +0530

    login logout functionality added

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,16 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'qwerty';
 
-app.use(cors({ credentials: true, origin: 'http://localhost:5173'}));
+app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb://127.0.0.1:27017/myblog-mern')
     .then(() => console.log('Connected!!'));
@@ -29,12 +32,12 @@ app.post('/register', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-    const { username, password} = req.body;
-    const userDoc = await User.findOne({username});
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({ username });
     const passOk = bcrypt.compareSync(password, userDoc.password)
     if (passOk) {
         // user authorised
-        jwt.sign({username, id: userDoc._id}, secret, {}, (err, token) => {
+        jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
             if (err) throw err;
             res.cookie('token', token).json('ok');
         })
@@ -42,6 +45,19 @@ app.post('/login', async (req, res) => {
         // send 400  error message
         res.status(400).json('Invalid credentials');
     }
+})
+
+app.get('/profile', (req, res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+         if (err) throw err;
+         res.json(info);
+    })
+})
+
+app.post('/logout', (req, res)=> {
+    res.cookie('token', '').json("ok");
+    
 })
 
 app.listen(4000);
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,47 @@
-import {Link} from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
 
 function Header() {
+
+    const [username, setUsername] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/profile', {
+            credentials: 'include'
+        }).then(response => {
+                response.json()
+                    .then(userInfo => {
+                        setUsername(userInfo.username)
+                    })
+            })
+    }, [])
+
+    const logout = () => {
+        fetch('http://localhost:4000/logout', {
+            credentials: 'include',
+            method: 'POST'
+        })
+        setUsername(null);
+    }
 
     return (
         <header>
             <Link to="/" className='logo'>MyBlog</Link>
             <nav>
-                <Link to="/login">LogIn</Link>
-                <Link to="/register">Register</Link>
+                {
+                    username && (
+                        <>
+                            <Link to={'/create'} >Create new post</Link>
+                            <a onClick={logout}>Logout</a>
+                        </>)
+                }
+                {
+                    !username && (
+                        <>
+                            <Link to="/login">LogIn</Link>
+                            <Link to="/register">Register</Link>
+                        </>)
+                }
             </nav>
         </header>
     )
